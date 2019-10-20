182ed8b066f17a94f9c35eabc76907185aca19c5
ArianeGomes <arianecgomes@hotmail.com>
2019-10-20 09:20:58 -0300

    Mudança nas imagens dos slides

--- src/components/Times.jsx
+++ src/components/Times.jsx
@@ -11,26 +11,54 @@ class Times extends Component {
           tarefas:
         </p>
         <ul>
+          <h5>
+            
+          </h5>
+          <p>
+            Responsável por transmitir o altruísmo, respeito e diversos outros
+            valores por meio de atividades de conscientização e ações práticas, impactando
+            diretamente os assistidos e sociedade como um todo e contagiando-as para que
+            também espalhem a magia do Natal por onde passarem.
+          </p>
+          <p>
+            - O que fazemos? Pit Stop, Operação Doe Sangue, Teatro, Atividades lúdicas
+          contínuas com todas as faixas etárias e muito mais.
+          </p>
+          <p>
+            - Carga Horária: 3 - 4 horas semanais
+          </p>
+          <p>
+            - Aprendizado: comunicação, trabalho em equipe, responsabilidade.
+          </p>
+          <p>
+          Coordenadores: Gabi e Nicolle.
+          </p>
           <li>
-            <h5>Arrecadação</h5>
+            <h5>ARRECADAÇÃO</h5>
             <p>
               Responsável por arrecadar todos os donativos para as instituições
               atendidas pela Operação Natal, organizando atividades que promovem
-              divulgação do projeto e o contato direto com população. - O que
-              fazemos? Dias de Mercado, Dia D, Abordagem contínua e novos tipos
-              de arrecadação. - Carga Horária: 2 - 3 horas semanais. -
-              Aprendizado: liderança, perda de timidez, comunicação.
+              divulgação do projeto e o contato direto com população.
+            </p>
+            <p>- O que fazemos? Dias de Mercado, Dia D, Abordagem contínua e novos tipos
+              de arrecadação.
+            </p>
+            <p>
+              - Carga Horária: 2 - 3 horas semanais. 
+            </p>
+            <p>- Aprendizado: liderança, perda de timidez, comunicação.
+            </p>
+            <p>
               Coordenadores: Matheus (Fantone) e Vitor (Double).
             </p>
           </li>
-          <li>Ação Social</li>
-          <li>Logística</li>
-          <li>Financeiro</li>
           <li>Eventos</li>
+          <li>Financeiro</li>
+          <li>Logística</li>
           <li>Marketing</li>
           <li>Qualidade</li>
+          <li>Recursos Humanos</li>
           <li>Sacolinhas</li>
-          <li>RH</li>
         </ul>
       </Container>
     );
