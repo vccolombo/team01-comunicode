19e8d9883fee207ec9490fe4818e244cd40b5a7a
ArianeGomes <arianecgomes@hotmail.com>
2019-10-20 10:17:42 -0300

    Menu Times aprimorado

--- src/components/Times.jsx
+++ src/components/Times.jsx
@@ -5,60 +5,224 @@ class Times extends Component {
   render() {
     return (
       <Container>
-        <h1>Times</h1>
+        <h4 style={{color: "#A23234", fontWeight:"bold"}}>TIMES</h4>
         <p>
           Estruturalmente, há atualmente 9 times entre os quais se dividem as
           tarefas:
         </p>
         <ul>
-          <h5>
-            
+          <br/>
+          <br/>
+
+          <h5 style={{color: 'white', backgroundColor: "#A23234", display: "inline", borderRadius: "5px", 
+          fontSize:"24px", borderStyle: "solid", borderColor: "#A23234", marginBottom: "10px"}}>
+            AÇÃO SOCIAL
           </h5>
+          <br/>
+          <br/>
           <p>
             Responsável por transmitir o altruísmo, respeito e diversos outros
             valores por meio de atividades de conscientização e ações práticas, impactando
             diretamente os assistidos e sociedade como um todo e contagiando-as para que
             também espalhem a magia do Natal por onde passarem.
           </p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>O que fazemos? </text> 
+            Pit Stop, Operação Doe Sangue, Teatro, Atividades lúdicas
+            contínuas com todas as faixas etárias e muito mais.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Carga horária: </text> 
+            3 - 4 horas semanais.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Aprendizado: </text> 
+            Comunicação, trabalho em equipe, responsabilidade.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Coordenadores: </text>
+            Gabi e Nicolle.</p>
+          <br/>
+          <br/>
+          
+          <h5 style={{color: 'white', backgroundColor: "#A23234", display: "inline", borderRadius: "5px", 
+          fontSize:"24px", borderStyle: "solid", borderColor: "#A23234", marginBottom: "10px"}}>
+            ARRECADAÇÃO
+          </h5>
+          <br/>
+          <br/>
           <p>
-            - O que fazemos? Pit Stop, Operação Doe Sangue, Teatro, Atividades lúdicas
-          contínuas com todas as faixas etárias e muito mais.
+            Responsável por arrecadar todos os donativos para as instituições
+            atendidas pela Operação Natal, organizando atividades que promovem divulgação
+            do projeto e o contato direto com população.
           </p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>O que fazemos? </text> 
+            Dias de Mercado, Dia D, Abordagem contínua e novos tipos
+            de arrecadação.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Carga horária: </text> 
+            2 - 3 horas semanais.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Aprendizado: </text> 
+            Liderança, perda de timidez, comunicação.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Coordenadores: </text>
+            Matheus (Fantone) e Vitor (Double).</p>
+          <br/>
+          <br/>
+            
+          <h5 style={{color: 'white', backgroundColor: "#A23234", display: "inline", borderRadius: "5px", 
+          fontSize:"24px", borderStyle: "solid", borderColor: "#A23234", marginBottom: "10px"}}>
+            EVENTOS
+          </h5>
+          <br/>
+          <br/>
           <p>
-            - Carga Horária: 3 - 4 horas semanais
+            Responsável por criar laços e proporcionar experiências marcantes entre
+            membros e assistidos, organizando atividades integrativas e eventos em instituições,
+            além de promover eventos beneficentes em estabelecimentos parceiros e oferecer
+            suporte para os maiores eventos do projeto.
           </p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>O que fazemos? </text> 
+            Visitas, Festinhas, Operação Trem Bão, Operação Kamzu e
+            muito mais.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Carga horária: </text> 
+            3 - 5 horas semanais.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Aprendizado: </text> 
+            Liderança, trabalho em equipe e técnicas de negociação.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Coordenadores: </text>
+            Alex e Vinicius (Berg).</p>
+          <br/>
+          <br/>
+
+          <h5 style={{color: 'white', backgroundColor: "#A23234", display: "inline", borderRadius: "5px", 
+          fontSize:"24px", borderStyle: "solid", borderColor: "#A23234", marginBottom: "10px"}}>
+            FINANCEIRO
+          </h5>
+          <br/>
+          <br/>
           <p>
-            - Aprendizado: comunicação, trabalho em equipe, responsabilidade.
+            Responsável pela captação de recursos e da saúde financeira do projeto,
+            além de contribuir para a realização dos sonhos de nossos assistidos.
           </p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>O que fazemos? </text> 
+            Prospecção de patrocínio e parcerias, Compra e cotação
+            de produtos, Comissão dos sonhos e Shark Tank do Fin.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Carga horária: </text> 
+            4 - 5 horas semanais.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Aprendizado: </text> 
+            Comunicação, técnicas de negociação, cumprimento de metas
+            e prazos.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Coordenadores: </text>
+            João e Aryanne.</p>
+          <br/>
+          <br/>
+
+          <h5 style={{color: 'white', backgroundColor: "#A23234", display: "inline", borderRadius: "5px", 
+          fontSize:"24px", borderStyle: "solid", borderColor: "#A23234", marginBottom: "10px"}}>
+            LOGÍSTICA
+          </h5>
+          <br/>
+          <br/>
           <p>
-          Coordenadores: Gabi e Nicolle.
+            Responsável pela comunicação e acompanhamento das instituições
+            assistidas pelo projeto, além de todo processamento dos donativos arrecadados.
           </p>
-          <li>
-            <h5>ARRECADAÇÃO</h5>
-            <p>
-              Responsável por arrecadar todos os donativos para as instituições
-              atendidas pela Operação Natal, organizando atividades que promovem
-              divulgação do projeto e o contato direto com população.
-            </p>
-            <p>- O que fazemos? Dias de Mercado, Dia D, Abordagem contínua e novos tipos
-              de arrecadação.
-            </p>
-            <p>
-              - Carga Horária: 2 - 3 horas semanais. 
-            </p>
-            <p>- Aprendizado: liderança, perda de timidez, comunicação.
-            </p>
-            <p>
-              Coordenadores: Matheus (Fantone) e Vitor (Double).
-            </p>
-          </li>
-          <li>Eventos</li>
-          <li>Financeiro</li>
-          <li>Logística</li>
-          <li>Marketing</li>
-          <li>Qualidade</li>
-          <li>Recursos Humanos</li>
-          <li>Sacolinhas</li>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>O que fazemos? </text> 
+            Galpão, Acompanhamento de instituições e Processamento
+            dos donativos arrecadados.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Carga horária: </text> 
+            4 - 5 horas semanais.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Aprendizado: </text> 
+            Comunicação, liderança, trabalho em equipe.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Coordenadores: </text>
+            Camila e Fábio.</p>
+          <br/>
+          <br/>
+
+          <h5 style={{color: 'white', backgroundColor: "#A23234", display: "inline", borderRadius: "5px", 
+          fontSize:"24px", borderStyle: "solid", borderColor: "#A23234", marginBottom: "10px"}}>
+            MARKETING
+          </h5>
+          <br/>
+          <br/>
+          <p>
+            Responsável por toda a divulgação do projeto para seus diferentes
+            públicos, através da produção de conteúdos audiovisuais digitais e físicos,
+            trabalhando diretamente com sua imagem e transmitindo seus valores e conquistas.
+          </p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>O que fazemos? </text> 
+            Artes e vídeos criativos, Estratégias de divulgação, Registro
+            audiovisual das atividades e muito mais.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Carga horária: </text> 
+            4 - 5 horas semanais.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Aprendizado: </text> 
+            Ferramentas de edição e tratamento audiovisual, criatividade e
+            comunicação.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Coordenadores: </text>
+            Isabella e Marco (Marquin).</p>
+          <br/>
+          <br/>
+
+          <h5 style={{color: 'white', backgroundColor: "#A23234", display: "inline", borderRadius: "5px", 
+          fontSize:"24px", borderStyle: "solid", borderColor: "#A23234", marginBottom: "10px"}}>
+            QUALIDADE
+          </h5>
+          <br/>
+          <br/>
+          <p>
+            Responsável pela aprendizagem organizacional e pela otimização dos
+            processos de negócio necessários para a execução das atividades do projeto, além
+            de identificar riscos e oportunidades em cada um.
+          </p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>O que fazemos? </text> 
+            Mapeamento de processos, Auditorias, Gestão do
+            conhecimento, Implementação de novas ideias e Feedbacks.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Carga horária: </text> 
+            3 - 4 horas semanais.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Aprendizado: </text> 
+            Visão crítica, trabalho em equipe e resolução de problemas.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Coordenadores: </text>
+            Felipe (Anache) e Milena.</p>
+          <br/>
+          <br/>
+
+          <h5 style={{color: 'white', backgroundColor: "#A23234", display: "inline", borderRadius: "5px", 
+          fontSize:"24px", borderStyle: "solid", borderColor: "#A23234", marginBottom: "10px"}}>
+            RECURSOS HUMANOS
+          </h5>
+          <br/>
+          <br/>
+          <p>
+            Responsável por cuidar dos membros do projeto,
+            promovendo atividades com objetivo de desenvolver, motivar e integrar os
+            envolvidos.
+          </p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>O que fazemos? </text> 
+            Gestão de pessoas, Integras, Internatal, Treinamentos e
+            Banco de dados.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Carga horária: </text> 
+            2 - 3,5 horas semanais.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Aprendizado: </text> 
+            Trabalho em equipe, lidar com pessoas e organização.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Coordenadores: </text>
+            Danilo e Leonardo (Sheetos)</p>
+          <br/>
+          <br/>
+
+          <h5 style={{color: 'white', backgroundColor: "#A23234", display: "inline", borderRadius: "5px", 
+          fontSize:"24px", borderStyle: "solid", borderColor: "#A23234", marginBottom: "10px"}}>
+            SACOLINHAS
+          </h5>
+          <br/>
+          <br/>
+          <p>
+            Responsável por proporcionar experiências únicas para as pessoas
+            com presentes de Natal, através do processamento de sacolinhas para os assistidos
+            e da Loja Solidária para a população carente.
+          </p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>O que fazemos? </text> 
+            Visitas para personalização dos cartões, Loja Solidária,
+            Acompanhamento do apadrinhamento de sacolinhas e Processamento das
+            sacolinhas recebidas.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Carga horária: </text> 
+            2 - 4 horas semanais.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Aprendizado: </text> 
+            Liderança, organização, capacidade de lidar com imprevistos.</p>
+          <p><text style={{color: "#A23234", fontWeight:"bold"}}>Coordenadores: </text>
+            Isadora e Marina.</p>
+          <br/>
+          <br/>
         </ul>
       </Container>
     );
